f252340792d9264737dd908c35985c25ee82e84c
NagBandla99 <64601255+NagBandla99@users.noreply.github.com>
2024-10-23 12:51:54 +0100

    added health check

--- src/Defra.Trade.Events.Services.CatchCertificates/Startup.cs
+++ src/Defra.Trade.Events.Services.CatchCertificates/Startup.cs
@@ -4,10 +4,14 @@
 using System.Diagnostics.CodeAnalysis;
 using Defra.Trade.Common.AppConfig;
 using Defra.Trade.Common.Config;
+using Defra.Trade.Common.Function.Health.HealthChecks;
 using Defra.Trade.Events.Services.CatchCertificates.Infrastructure;
 using Defra.Trade.Events.Services.CatchCertificates.Logic;
 using Defra.Trade.Events.Services.CatchCertificates.Logic.Configuration;
+using FunctionHealthCheck;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 [assembly: FunctionsStartup(typeof(Defra.Trade.Events.Services.CatchCertificates.Startup))]
 
@@ -19,7 +23,8 @@ public class Startup : FunctionsStartup
     public override void Configure(IFunctionsHostBuilder builder)
     {
         var configuration = builder.GetContext().Configuration;
-
+        var healthChecksBuilder = builder.Services.AddFunctionHealthChecks();
+        RegisterHealthChecks(healthChecksBuilder, configuration);
         builder.Services.AddServiceRegistrations(configuration);
     }
 
@@ -35,5 +40,13 @@ public class Startup : FunctionsStartup
                 opt.Select<ApimConfiguration>(ApimConfiguration.OptionsName);
                 opt.ConfigServer.Select(ApplicationConstants.AppName);
             });
+    }
+
+    private static void RegisterHealthChecks(
+        IHealthChecksBuilder builder,
+        IConfiguration configuration)
+    {
+        builder.AddCheck<AppSettingHealthCheck>("ServiceBus:ConnectionString");
+        builder.AddAzureServiceBusCheck(configuration, "ServiceBus:ConnectionString", ApplicationConstants.ServiceBus.QueueName.CatchCertificatesCreate);
     }
 }
